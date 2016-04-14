47a370e1fb46fcecad15d565e3b89a2c298d9785
Carmelo Naciti <carmelon@miway.co.za>
2016-04-14 16:20:37 +0200

    Added @ORM

--- src/AppBundle/Entity/MediaObject.php
+++ src/AppBundle/Entity/MediaObject.php
@@ -2,34 +2,68 @@
 
 namespace AppBundle\Entity;
 
-use Doctrine\ORM\Mapping;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Class MediaObject
- *
- * @package AppBundle\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="qa_content")
  */
 class MediaObject
 {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $title;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $description;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $tags;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $team;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $environment;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $story;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $type;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $user;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $views;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $timestamp;
 }
