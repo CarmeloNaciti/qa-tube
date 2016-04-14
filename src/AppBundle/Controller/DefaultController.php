878199f60e78322887d0e8e1c5a16ab97167892c
Carmelo Naciti <carmelon@miway.co.za>
2016-04-14 15:52:56 +0200

    Amended App config

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,6 +9,16 @@ class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
+        echo 'Index';
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+
+    public function testAction()
+    {
+        echo 'Test';
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
