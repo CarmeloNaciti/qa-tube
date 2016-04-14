c70f3210654188fb15fe66b943334b3ba5aa6f75
Le-Roux Enslin <le-rouxe@miway.co.za>
2016-04-14 16:53:12 +0200

    Generated getters and settrs for MediaObject

--- src/AppBundle/Entity/MediaObject.php
+++ src/AppBundle/Entity/MediaObject.php
@@ -66,4 +66,254 @@ class MediaObject
      * @ORM\Column(type="string", length=100)
      */
     protected $timestamp;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return MediaObject
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return MediaObject
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set tags
+     *
+     * @param string $tags
+     *
+     * @return MediaObject
+     */
+    public function setTags($tags)
+    {
+        $this->tags = $tags;
+
+        return $this;
+    }
+
+    /**
+     * Get tags
+     *
+     * @return string
+     */
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    /**
+     * Set team
+     *
+     * @param string $team
+     *
+     * @return MediaObject
+     */
+    public function setTeam($team)
+    {
+        $this->team = $team;
+
+        return $this;
+    }
+
+    /**
+     * Get team
+     *
+     * @return string
+     */
+    public function getTeam()
+    {
+        return $this->team;
+    }
+
+    /**
+     * Set environment
+     *
+     * @param string $environment
+     *
+     * @return MediaObject
+     */
+    public function setEnvironment($environment)
+    {
+        $this->environment = $environment;
+
+        return $this;
+    }
+
+    /**
+     * Get environment
+     *
+     * @return string
+     */
+    public function getEnvironment()
+    {
+        return $this->environment;
+    }
+
+    /**
+     * Set story
+     *
+     * @param string $story
+     *
+     * @return MediaObject
+     */
+    public function setStory($story)
+    {
+        $this->story = $story;
+
+        return $this;
+    }
+
+    /**
+     * Get story
+     *
+     * @return string
+     */
+    public function getStory()
+    {
+        return $this->story;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return MediaObject
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set user
+     *
+     * @param string $user
+     *
+     * @return MediaObject
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return string
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set views
+     *
+     * @param string $views
+     *
+     * @return MediaObject
+     */
+    public function setViews($views)
+    {
+        $this->views = $views;
+
+        return $this;
+    }
+
+    /**
+     * Get views
+     *
+     * @return string
+     */
+    public function getViews()
+    {
+        return $this->views;
+    }
+
+    /**
+     * Set timestamp
+     *
+     * @param string $timestamp
+     *
+     * @return MediaObject
+     */
+    public function setTimestamp($timestamp)
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+
+    /**
+     * Get timestamp
+     *
+     * @return string
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
 }
